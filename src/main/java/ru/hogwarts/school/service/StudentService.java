f09b5d740bb16ef6244022d8cfba0512bec19bdc
Pavlen <pavlenovm@inbox.ru>
2026-07-21 14:25:58 +0300

    Create need method and add class InfoController

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,6 +13,7 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private static final Logger logger = LoggerFactory.getLogger(StudentService.class);
     private final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
@@ -18,51 +21,80 @@ public class StudentService {
     }
 
     public Student saveStudent(Student student) {
+        logger.info("Was invoked method for create student");
+        logger.debug("Creating student with name={}, age={}", student.getName(), student.getAge());
         return studentRepository.save(student);
     }
 
     public Student getStudentById(Long id) {
+        logger.info("Was invoked method for get student by id");
+        logger.debug("Fetching student with id={}", id);
+
         return studentRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND, "Student not found with id: " + id));
+                .orElseThrow(() -> {
+                    String msg = "There is no student with id = " + id;
+                    logger.error(msg);
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, msg);
+                });
     }
 
     public Student getStudentWithFaculty(Long id) {
+        logger.info("Was invoked method for get student with faculty");
+        logger.debug("Fetching student with faculty, id={}", id);
+
         Student student = studentRepository.findByIdWithFaculty(id);
         if (student == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found with id: " + id);
+            String msg = "There is no student with id = " + id + " (with faculty)";
+            logger.error(msg);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, msg);
         }
         return student;
     }
 
     public List<Student> getAllStudents() {
+        logger.info("Was invoked method for get all students");
         return studentRepository.findAll();
     }
 
     public void deleteStudent(Long id) {
+        logger.info("Was invoked method for delete student");
+        logger.debug("Deleting student with id={}", id);
+
         if (!studentRepository.existsById(id)) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "Student not found with id: " + id);
+            String msg = "There is no student with id = " + id + " to delete";
+            logger.error(msg);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, msg);
         }
         studentRepository.deleteById(id);
     }
 
     public List<Student> getStudentsByAgeRange(int minAge, int maxAge) {
+        logger.info("Was invoked method for get students by age range");
+        logger.debug("Searching students with age between {} and {}", minAge, maxAge);
+
         if (minAge > maxAge) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "minAge cannot be greater than maxAge");
+            String msg = "Invalid age range: minAge (" + minAge + ") cannot be greater than maxAge (" + maxAge + ")";
+            logger.warn(msg);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, msg);
         }
         return studentRepository.findByAgeBetween(minAge, maxAge);
     }
+
     public long getTotalStudentsCount() {
+        logger.info("Was invoked method for get total students count");
         return studentRepository.countAllStudents();
     }
 
     public double getAverageAge() {
+        logger.info("Was invoked method for get average age");
         Double avg = studentRepository.averageAge();
-        return (avg == null) ? 0.0 : avg;
+        double result = (avg == null) ? 0.0 : avg;
+        logger.debug("Calculated average age={}", result);
+        return result;
     }
 
     public List<Student> getLastFiveStudents() {
+        logger.info("Was invoked method for get last five students");
         return studentRepository.findLastFiveStudents();
     }
 }
